638016da8021d05f6ab2ff801ab3f599b3509f4d
Joan Chen <joanchenuk@gmail.com>
2022-08-31 10:53:50 +0100

    Remove return and add type annotatons

--- s3.py
+++ s3.py
@@ -5,14 +5,10 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-def upload_to_s3(file_name, bucket_name, object_name):
-    """ Upload a file to an S3 bucket """
-
-    # Upload the file
+def upload_to_s3(file_name: str, bucket_name: str, object_name: str) -> None:
+    """Upload a file to an S3 bucket"""
     s3_client = boto3.client("s3")
     try:
         s3_client.upload_file(file_name, bucket_name, object_name)
     except ClientError as error:
         logging.error(error)
-        return False
-    return True
